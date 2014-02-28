bf3845c46356c16d969c945e3f91de8d81a26b6c
Joschi Kuphal <joschi@kuphal.net>
2014-02-28 20:23:53 +0100

    PNG creation & optimization

--- Gruntfile.js
+++ Gruntfile.js
@@ -16,16 +16,16 @@ module.exports = function(grunt) {
       all: [
         'Gruntfile.js',
         'tasks/*.js',
-        '<%= nodeunit.tests %>',
+        '<%= nodeunit.tests %>'
       ],
       options: {
-        jshintrc: '.jshintrc',
+        jshintrc: '.jshintrc'
       },
     },
 
     // Before generating any new files, remove any previously-created files.
     clean: {
-      tests: ['tmp'],
+      tests: ['tmp']
     },
 
     // Configuration to be run (and then tested).
@@ -34,24 +34,24 @@ module.exports = function(grunt) {
         options: {
         },
         files: {
-          'tmp/default_options': ['test/fixtures/testing', 'test/fixtures/123'],
-        },
+          'tmp/default_options': ['test/fixtures/testing', 'test/fixtures/123']
+        }
       },
       custom_options: {
         options: {
           separator: ': ',
-          punctuation: ' !!!',
+          punctuation: ' !!!'
         },
         files: {
-          'tmp/custom_options': ['test/fixtures/testing', 'test/fixtures/123'],
-        },
-      },
+          'tmp/custom_options': ['test/fixtures/testing', 'test/fixtures/123']
+        }
+      }
     },
 
     // Unit tests.
     nodeunit: {
-      tests: ['test/*_test.js'],
-    },
+      tests: ['test/*_test.js']
+    }
 
   });
 
--- tasks/iconizr.js
+++ tasks/iconizr.js
@@ -9,42 +9,33 @@
 'use strict';
 
 module.exports = function(grunt) {
-
-  // Please see the Grunt documentation for more information regarding task
-  // creation: http://gruntjs.com/creating-tasks
-
-  grunt.registerMultiTask('iconizr', 'Grunt plugin that takes a folder of SVG icons and creates a CSS icon kit out of them, including SVG & PNG sprites, suitable CSS / Sass / LESS etc. resources and a JavaScript loader for easy integration into your HTML documents', function() {
-    // Merge task-specific and/or target-specific options with these defaults.
-    var options = this.options({
-      punctuation: '.',
-      separator: ', '
-    });
-
-    // Iterate over all specified file groups.
-    this.files.forEach(function(f) {
-      // Concat specified files.
-      var src = f.src.filter(function(filepath) {
-        // Warn on and remove invalid source files (if nonull was set).
-        if (!grunt.file.exists(filepath)) {
-          grunt.log.warn('Source file "' + filepath + '" not found.');
-          return false;
-        } else {
-          return true;
-        }
-      }).map(function(filepath) {
-        // Read file source.
-        return grunt.file.read(filepath);
-      }).join(grunt.util.normalizelf(options.separator));
-
-      // Handle options.
-      src += options.punctuation;
-
-      // Write the destination file.
-      grunt.file.write(f.dest, src);
-
-      // Print a success message.
-      grunt.log.writeln('File "' + f.dest + '" created.');
-    });
-  });
-
+	var SVGSprite			= require('svg-sprite'),
+	util					= require('util'),
+	chalk					= require('chalk'),
+	path					= require('path'),
+	iconizr					= require(path.join('..', 'lib', 'iconizr.js'));
+	
+	grunt.registerMultiTask('iconizr', 'Grunt plugin that takes a folder of SVG icons and creates a CSS icon kit out of them, including SVG & PNG sprites, suitable CSS / Sass / LESS etc. resources and a JavaScript loader for easy integration into your HTML documents', function() {
+		var options			= this.options(),
+		that				= this;
+		options._keep		= options.keep;
+		options.keep		= 1;
+		
+		this.files.forEach(function(file) {
+			var indir		= file.src.shift() || './',
+			outdir			= util.isArray(file.dest) ? file.dest.shift() : ('' + file.dest),
+			done			= that.async();
+			
+			grunt.log.writeln('Building SVG sprite from directory "%s" ...', indir);
+			
+			SVGSprite.createSprite(indir, outdir, options, function(error, results) {
+				if (error) {
+					console.error(error);
+					done();
+				} else {
+					iconizr.createIconKit(results, done);
+				}
+			});
+		});
+	});
 };
